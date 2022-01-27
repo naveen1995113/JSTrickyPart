f0608629cdc0bcdf6c6c673a291058509f82909d
Chintakindi Naveen <cnaveen@aligntech.com>
2022-01-27 20:55:05 +0530

    feat: deleted unncessary files

--- Scope&Hoisting/one.js
+++
@@ -1,2 +0,0 @@
-var myName = "Max";
-const myAge = 31;
--- Scope&Hoisting/two.js
+++
@@ -1,8 +0,0 @@
-console.log(myName); // Max , defined with var hence added to global object/context
-console.log(this.myName); // Max
-console.log(self.myName); // Max
-
-console.log(myAge); // 31 , defined with const hence added to global scope
-console.log(this.myAge); // Error ,
-// defined with const hence added to global scope,but not added to global object/context
-console.log(self.myAge); // Error
